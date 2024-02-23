5ca9ecfb82b9b11c48a4172e2eb5ca1a227974c5
ShubhDPatel <shubh09226@gmail.com>
2024-02-23 14:43:43 -0500

    Add header comment for altrium.cpp

---
+++ src/altrium.cpp
@@ -0,0 +1,7 @@
+/*
+*   altrium.cpp
+*
+*   Main entry point for the altrium application.
+*   Launches a website with user specified options.
+*   Such options include: a website name, query strings, and others.
+*/
