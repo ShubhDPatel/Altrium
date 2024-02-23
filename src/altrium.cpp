19c4cc1544c0abfcc2304f540d296fe73c196e6e
ShubhDPatel <shubh09226@gmail.com>
2024-02-23 14:54:30 -0500

    Remove spaces in header comment

--- src/altrium.cpp
+++ src/altrium.cpp
@@ -1,7 +1,7 @@
 /*
-*   altrium.cpp
+* altrium.cpp
 *
-*   Main entry point for the altrium application.
-*   Launches a website with user specified options.
-*   Such options include: a website name, query strings, and others.
+* Main entry point for the altrium application.
+* Launches a website with user specified options.
+* Such options include: a website name, query strings, and others.
 */
