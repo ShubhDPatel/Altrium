1b9a6df68e91fde45d3476d2def205ed6e0f4d51
ShubhDPatel <shubh09226@gmail.com>
2024-03-08 14:37:20 -0500

    Add CLI arguments into main()

--- src/altrium.cpp
+++ src/altrium.cpp
@@ -1,11 +1,12 @@
 /*
-* altrium.cpp
-*
-* Main entry point for the altrium application.
-* Launches a website with user specified options.
-* Such options include: a website name, query strings, and others.
-*/
+ * altrium.cpp
+ *
+ * Main entry point for the altrium application.
+ * Launches a website with user specified options.
+ * Such options include: a website name, query strings, and others.
+ */
 
-int main() {
+int main(int argc, char* argv[])
+{
     return 0;
 }
