c83d92a6c1f4a78f5e31c23bfdb1e34644ffcf2f
ShubhDPatel <shubh09226@gmail.com>
2024-03-08 23:05:34 -0500

    Add Google class

--- src/libAltrium/websites/Google.hpp
+++ src/libAltrium/websites/Google.hpp
@@ -7,5 +7,16 @@
 #ifndef GOOGLE_HPP
 #define GOOGLE_HPP
 
+#include "Website.hpp"
+
+class Google : public Website
+{
+public:
+    /*
+    * Builds the query string for the website.
+    */
+    void buildQuery() override;
+};
+
 #endif
 
