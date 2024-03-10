cc42e0f0fac669fb895ab87aa97d4a46e19ac680
ShubhDPatel <shubh09226@gmail.com>
2024-03-10 17:32:14 -0400

    Add constructor def

--- src/libAltrium/websites/StackOverflow.cpp
+++ src/libAltrium/websites/StackOverflow.cpp
@@ -3,3 +3,19 @@
 *
 * Implementation of the StackOverflow class
 */
+
+#include "StackOverflow.hpp"
+
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+
+/*
+* Constructor
+*
+* @param[in] query string that contains the query
+*/
+Google::Google(const std::string query)
+{
+    url = query;
+}
